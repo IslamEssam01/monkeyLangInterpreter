aff68a65b11fee496a0615bb4ad497bc487b17b5
IslamEssam01 <essamislam260105@gmail.com>
2024-06-28 02:28:36 +0300

    ended the parser and started working on the evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey/evaluator"
 	"monkey/lexer"
-	"monkey/token"
+	"monkey/parser"
+	"reflect"
 )
 
 const PROMPT = ">> "
@@ -22,8 +24,40 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		evaluated := evaluator.Eval(program)
+
+		if !reflect.ValueOf(evaluated).IsNil() {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+// const MONKEY_FACE = `            __,__
+//    .--.  .-"     "-.  .--.
+//   / .. \/  .-. .-.  \/ .. \
+//  | |  '|  /   Y   \  |'  | |
+//  | \   \  \ 0 | 0 /  /   / |
+//   \ '- ,\.-"""""""-./, -' /
+//    ''-' /_   ^ ^   _\ '-''
+//        |  \._   _./  |
+//        \   \ '~' /   /
+//         '._ '-=-' _.'
+//            '-----'
+//     `
+
+func printParserErrors(out io.Writer, errors []string) {
+	// io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
